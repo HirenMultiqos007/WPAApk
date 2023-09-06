7db05a7f7f9fd55b56baab57ead8bd6c907f1d8e
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-06 10:04:35 +0530

    aaads

--- public/sw.js
+++ public/sw.js
@@ -8,73 +8,73 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-    if(!navigator.onLine){
-        event.respondWith(
-          caches.match(event.request).then((result) => {
-            if (result) {
-              return result;
-            }
-            let requestUrl = event.request.clone();
-            return fetch(requestUrl);
-          })
-        );
-    }
-});
-
-// This variable will save the event for later use.
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevents the default mini-infobar or install dialog from appearing on mobile
-  e.preventDefault();
-  // Save the event because you'll need to trigger it later.
-  deferredPrompt = e;
-  showInAppInstallPromotion();
-});
-
-
-// Gather the data from your custom install UI event listener
-installButton.addEventListener('click', async () => {
-  // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
-  deferredPrompt.prompt();
-  // Find out whether the user confirmed the installation or not
-  const { outcome } = await deferredPrompt.userChoice;
-  // The deferredPrompt can only be used once.
-  deferredPrompt = null;
-  // Act on the user's choice
-  if (outcome === 'accepted') {
-    console.log('User accepted the install prompt.');
-  } else if (outcome === 'dismissed') {
-    console.log('User dismissed the install prompt');
-  }
-});
-
 // self.addEventListener("fetch", (event) => {
 //     if(!navigator.onLine){
 //         event.respondWith(
-//           caches.match(event.request).then((cachedResponse) => {
-//             if (cachedResponse) {
-//               return cachedResponse;
+//           caches.match(event.request).then((result) => {
+//             if (result) {
+//               return result;
 //             }
-
-//             // If the request is not in cache, fetch it from the network
-//             return fetch(event.request)
-//               .then((response) => {
-//                 // Clone the response as it can only be consumed once
-//                 const responseClone = response.clone();
-
-//                 // Cache the response for future requests
-//                 caches.open(cacheData).then((cache) => {
-//                   cache.put(event.request, responseClone);
-//                 });
-
-//                 return response;
-//               })
-//               .catch((error) => {
-//                 console.log(error)
-//             //   return  caches.match(event.request)
-//               });
+//             let requestUrl = event.request.clone();
+//             return fetch(requestUrl);
 //           })
 //         );
 //     }
-//   });
+// });
+
+// This variable will save the event for later use.
+// let deferredPrompt;
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevents the default mini-infobar or install dialog from appearing on mobile
+//   e.preventDefault();
+//   // Save the event because you'll need to trigger it later.
+//   deferredPrompt = e;
+//   showInAppInstallPromotion();
+// });
+
+
+// // Gather the data from your custom install UI event listener
+// installButton.addEventListener('click', async () => {
+//   // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
+//   deferredPrompt.prompt();
+//   // Find out whether the user confirmed the installation or not
+//   const { outcome } = await deferredPrompt.userChoice;
+//   // The deferredPrompt can only be used once.
+//   deferredPrompt = null;
+//   // Act on the user's choice
+//   if (outcome === 'accepted') {
+//     console.log('User accepted the install prompt.');
+//   } else if (outcome === 'dismissed') {
+//     console.log('User dismissed the install prompt');
+//   }
+// });
+
+self.addEventListener("fetch", (event) => {
+    if(!navigator.onLine){
+        event.respondWith(
+          caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+              return cachedResponse;
+            }
+
+            // If the request is not in cache, fetch it from the network
+            return fetch(event.request)
+              .then((response) => {
+                // Clone the response as it can only be consumed once
+                const responseClone = response.clone();
+
+                // Cache the response for future requests
+                caches.open(cacheData).then((cache) => {
+                  cache.put(event.request, responseClone);
+                });
+
+                return response;
+              })
+              .catch((error) => {
+                console.log(error)
+            //   return  caches.match(event.request)
+              });
+          })
+        );
+    }
+  });
